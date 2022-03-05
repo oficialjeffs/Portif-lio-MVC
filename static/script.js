902a5a66b98b6c4323b93f28fe6ba1a5ca5cf806
oficialjeffs <jeffsantospersonal2@gmail.com>
2022-03-05 03:06:16 -0300

    Animação em todas as seções

--- static/script.js
+++ static/script.js
@@ -25,3 +25,26 @@ navItem.forEach(item => {
     })
 
 })
+
+// Animar todos os itens na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    console.log(windowTop)
+
+item.forEach(Element=>{
+    if (windowTop > Element.offsetTop){
+        Element.classList.add('animate');
+    } else {
+        Element.classList.remove('animate');
+    }
+});
+};
+
+animeScroll()
+
+window.addEventListener("scroll",()=>{
+    animeScroll();
+})
